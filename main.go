23f354f6bff08ad009bbe57f756f4656d6fa5fb8
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-09-13 13:28:11 -0300

    feature: adds creation and listing of all events

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gentil-eilison/events-booking-go/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Logger - logs operations of the server
+	// Recovery - makes sure only part of the server app crashses when panicking
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080") // Will run on localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, models.GetAllEvents())
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.UserID = 1
+	event.ID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+}
---
+++ models/event.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID int
+	Name string `binding:"required"`
+	Description string `binding:"required"`
+	Location string `binding:"required"`
+	DateTime time.Time `binding:"required"`
+	UserID int
+}
+
+var events = []Event{}
+
+func (event Event) Save() {
+	events = append(events, event)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
